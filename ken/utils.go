935512179f51cca43ac1e6f0dcf2e536e965e46c
raglandcodes <raglandasir@gmail.com>
2020-03-20 23:10:15 +0530

    fix CORS

--- ken/utils.go
+++ ken/utils.go
@@ -4,7 +4,11 @@ import "github.com/gin-gonic/gin"
 
 //SendResponse handles sendign response to client
 func SendResponse(c *gin.Context, status string, message interface{}) {
-	c.Header("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET")
+
 	// c.Next()
 	c.JSON(200, gin.H{
 		"status":  status,
